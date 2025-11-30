7d40cf58f89c0de95104d801657bc32edd827576
Emad Salim. <62475696+kxxjxxl@users.noreply.github.com>
2025-11-30 10:51:52 -0330

    Add chunking configuration for file upload

---
+++ chunkingConfig.py
@@ -0,0 +1,12 @@
+operation = client.file_search_stores.upload_to_file_search_store(
+    file_search_store_name=file_search_store.name,
+    file_name=sample_file.name,
+    config={
+        'chunking_config': {
+          'white_space_config': {
+            'max_tokens_per_chunk': 200,
+            'max_overlap_tokens': 20
+          }
+        }
+    }
+)
