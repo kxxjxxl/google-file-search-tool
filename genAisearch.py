3aab40e4cb5ddd02089106b95ceacbf2a895f6c1
Emad Salim. <62475696+kxxjxxl@users.noreply.github.com>
2025-11-30 10:50:31 -0330

    Implement file upload and content generation using GenAI

---
+++ genAisearch.py
@@ -0,0 +1,35 @@
+rom google import genai
+from google.genai import types
+import time
+
+client = genai.Client()
+
+# File name will be visible in citations
+sample_file = client.files.upload(file='sample.txt', config={'name': 'display_file_name'})
+
+file_search_store = client.file_search_stores.create(config={'display_name': 'your-fileSearchStore-name'})
+
+operation = client.file_search_stores.import_file(
+    file_search_store_name=file_search_store.name,
+    file_name=sample_file.name
+)
+
+while not operation.done:
+    time.sleep(5)
+    operation = client.operations.get(operation)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="""Can you tell me about [insert question]""",
+    config=types.GenerateContentConfig(
+        tools=[
+            types.Tool(
+                file_search=types.FileSearch(
+                    file_search_store_names=[file_search_store.name]
+                )
+            )
+        ]
+    )
+)
+
+print(response.text)
